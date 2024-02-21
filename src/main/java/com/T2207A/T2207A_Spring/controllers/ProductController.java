00567aeb4abb0191a17e77d136aa59203becb894
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-02-21 14:19:43 +0700

    service

--- src/main/java/com/T2207A/T2207A_Spring/controllers/ProductController.java
+++ src/main/java/com/T2207A/T2207A_Spring/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.T2207A.T2207A_Spring.controllers;
 import com.T2207A.T2207A_Spring.entities.Product;
 import com.T2207A.T2207A_Spring.models.ResponseObject;
 import com.T2207A.T2207A_Spring.repositories.ProductRepository;
+import com.T2207A.T2207A_Spring.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,73 +14,28 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/api/products")
+@RequestMapping(path = "/api/v1/products")
 public class ProductController {
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
 
-    @GetMapping("")
+    @GetMapping()
     List<Product> getAllProducts() {
-        return productRepository.findAll();
+        return productService.getAll();
     }
 
-    @GetMapping("/{id}")
-    ResponseEntity<ResponseObject> findById(@PathVariable Long id) {
-        Optional<Product> product = productRepository.findById(id);
-        if (product.isPresent()){
-            return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject("true", "Ok", product)
-            );
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    new ResponseObject("false", "Can't not find product with id "+id, "")
-            );
-        }
+    @PostMapping()
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
     }
 
-    @PostMapping("/insert")
-    ResponseEntity<ResponseObject> insertProduct(@RequestBody Product model) {
-        List<Product> foundProducts = productRepository.findByName(model.getName());
-        if (foundProducts.size() > 0){
-            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
-                    new ResponseObject("false", "Product name already taken", "")
-            );
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponseObject("true", "Ok", productRepository.save(model))
-        );
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product){
+        return productService.updateProduct(id,product);
     }
 
-    @PutMapping("/update/{id}")
-    ResponseEntity<ResponseObject> updateProduct(@RequestBody Product model, @PathVariable Long id) {
-        Product updateProduct = productRepository.findById(id)
-                .map(product -> {
-                    product.setName(model.getName());
-                    product.setPrice(model.getPrice());
-                    product.setDescription(model.getDescription());
-                    product.setThumbnail(model.getThumbnail());
-                    product.setQty(model.getQty());
-                    return productRepository.save(product);
-                }).orElseGet(() -> {
-                    model.setId(id);
-                    return productRepository.save(model);
-                });
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponseObject("true", "Update Product successfully", updateProduct)
-        );
-    }
-    @DeleteMapping("/delete/{id}")
-    ResponseEntity<ResponseObject> deleteProduct(@PathVariable Long id) {
-        boolean exists = productRepository.existsById(id);
-        if (exists){
-            productRepository.deleteById(id);
-            return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject("Ok", "Delete Product successfully", "")
-            );
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                    new ResponseObject("False", "Not Found", "")
-            );
-        }
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
     }
 }
---
+++ src/main/java/com/T2207A/T2207A_Spring/services/ProductService.java
@@ -0,0 +1,49 @@
+package com.T2207A.T2207A_Spring.services;
+
+import com.T2207A.T2207A_Spring.entities.Product;
+import com.T2207A.T2207A_Spring.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+//    public ProductService(ProductRepository productRepository) {
+//        this.productRepository = productRepository;
+//    }
+
+    public List<Product> getAll(){
+        return productRepository.findAll();
+    }
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product product){
+        return productRepository.findById(id)
+                .map(p-> {
+                    p.setName(product.getName());
+                    p.setPrice(product.getPrice());
+                    p.setQty(product.getQty());
+                    p.setThumbnail(product.getThumbnail());
+                    p.setDescription(product.getDescription());
+                    return productRepository.save(p);
+                })
+                .orElseGet(()->{
+                    product.setId(id);
+                    return productRepository.save(product);
+                });
+    }
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+
+
+
+}
